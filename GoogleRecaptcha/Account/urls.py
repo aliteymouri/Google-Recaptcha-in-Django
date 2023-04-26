4e76f0918103153b118add1c24c4447f1320bf5f
Ali Teymouri <aliteymr@gmail.com>
2023-04-26 19:53:06 +0330

    Delete GoogleRecaptcha directory

--- GoogleRecaptcha/Account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.views import LogoutView
-
-app_name = 'account'
-urlpatterns = [
-    path('log_in', views.LoginView.as_view(), name='log_in'),
-    path('log_out', LogoutView.as_view(next_page='home:home'), name='log_out'),
-]
